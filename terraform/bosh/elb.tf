ed1a9acbd6c74ea297f7d0ccb983c1fe183d3d3e
Timothy Mower <timothy.mower@gmail.com>
2016-02-25 15:29:28 +0000

    Add elb for bosh

---
+++ terraform/bosh/elb.tf
@@ -0,0 +1,59 @@
+resource "aws_elb" "bosh" {
+  name = "${var.env}-bosh"
+  subnets = ["${split(",", var.infra_subnet_ids)}"]
+  idle_timeout = 600
+  cross_zone_load_balancing = "true"
+  internal = "true"
+  security_groups = [
+    "${aws_security_group.bosh_elb.id}",
+  ]
+
+  health_check {
+    target = "TCP:22"
+    interval = 5
+    timeout = 2
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+  }
+  listener {
+    instance_port = 22
+    instance_protocol = "tcp"
+    lb_port = 22
+    lb_protocol = "tcp"
+  }
+
+  listener {
+    instance_port = 6868
+    instance_protocol = "tcp"
+    lb_port = 6868
+    lb_protocol = "tcp"
+  }
+
+  listener {
+    instance_port = 25555
+    instance_protocol = "tcp"
+    lb_port = 25555
+    lb_protocol = "tcp"
+  }
+
+  listener {
+    instance_port = 4222
+    instance_protocol = "tcp"
+    lb_port = 4222
+    lb_protocol = "tcp"
+  }
+
+  listener {
+    instance_port = 25250
+    instance_protocol = "tcp"
+    lb_port = 25250
+    lb_protocol = "tcp"
+  }
+
+  listener {
+    instance_port = 25777
+    instance_protocol = "tcp"
+    lb_port = 25777
+    lb_protocol = "tcp"
+  }
+}
--- terraform/bosh/security_groups.tf
+++ terraform/bosh/security_groups.tf
@@ -15,6 +15,78 @@ resource "aws_security_group" "bosh" {
     to_port   = 22
     protocol  = "tcp"
     security_groups = [
+      "${aws_security_group.bosh_elb.id}",
+    ]
+  }
+
+  ingress {
+    from_port = 6868
+    to_port   = 6868
+    protocol  = "tcp"
+    security_groups = [
+      "${aws_security_group.bosh_elb.id}",
+    ]
+  }
+
+  ingress {
+    from_port = 25555
+    to_port   = 25555
+    protocol  = "tcp"
+    security_groups = [
+      "${aws_security_group.bosh_elb.id}",
+    ]
+  }
+
+  ingress {
+    from_port = 4222
+    to_port   = 4222
+    protocol  = "tcp"
+    security_groups = [
+      "${aws_security_group.bosh_elb.id}",
+    ]
+  }
+
+  ingress {
+    from_port = 25250
+    to_port   = 25250
+    protocol  = "tcp"
+    security_groups = [
+      "${aws_security_group.bosh_elb.id}",
+    ]
+  }
+
+
+  ingress {
+    from_port = 25777
+    to_port   = 25777
+    protocol  = "tcp"
+    security_groups = [
+      "${aws_security_group.bosh_elb.id}",
+    ]
+  }
+
+  tags {
+    Name = "${var.env}-bosh"
+  }
+}
+
+resource "aws_security_group" "bosh_elb" {
+  name = "${var.env}-bosh-elb"
+  description = "Bosh elb security group"
+  vpc_id = "${var.vpc_id}"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    security_groups = [
       "${var.concourse_security_group_id}"
     ]
   }
