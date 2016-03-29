d8cb5bc39fb950b985a8cd619b119458adc1d977
Michal Tekel <mtekel@gmail.com>
2016-03-29 11:13:01 +0100

    A script to install & run nmap

---
+++ concourse/scripts/install-userspace-nmap.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -eu
+dst=$($DEBUG ? "" : ">/dev/null")
+
+mkdir -p portable/debs
+cd portable/debs
+apt-get download nmap liblua5.2 liblinear1 libblas3
+cd ..
+for file in debs/*.deb
+    do
+        ar x $file && tar xvf data.tar.* && rm control.tar.* && rm data.tar.* && rm debian-binary
+    done
+cd ..
+export LD_LIBRARY_PATH=$HOME/portable/usr/lib/:$HOME/portable/usr/lib/x86_64-linux-gnu:$HOME/portable/usr/lib/libblas
+export PATH=$PATH:$HOME/portable/usr/bin/
