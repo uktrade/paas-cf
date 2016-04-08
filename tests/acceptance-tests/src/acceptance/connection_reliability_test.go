47e2dba815d7e3c203dc2bb330b65fc603376624
Colin Saliceti <c.saliceti@gmail.com>
2016-04-08 15:39:08 +0100

    Allow selectively testing start/stop for pipeline split

--- tests/acceptance-tests/src/acceptance/connection_reliability_test.go
+++ tests/acceptance-tests/src/acceptance/connection_reliability_test.go
@@ -7,12 +7,17 @@ import (
 
 	"github.com/cloudfoundry-incubator/cf-test-helpers/cf"
 	"github.com/cloudfoundry-incubator/cf-test-helpers/helpers"
+	"os"
 )
 
 var _ = Describe("Connection reliability", func() {
-    appName := "connection-reliability"
+	appName := "connection-reliability"
 
 	It("should start testing successfully", func() {
+		if os.Getenv("DISABLE_ENV_SETUP") != "" {
+			Skip("Not testing start as connection reliability test is probably stopping")
+		}
+
 		Expect(cf.Cf(
 			"push", appName,
 			"-b", config.GoBuildpackName,
@@ -26,6 +31,9 @@ var _ = Describe("Connection reliability", func() {
 	})
 
 	It("should end testing and indicate that no connections failed", func() {
+		if os.Getenv("DISABLE_ENV_TEARDOWN") != "" {
+			Skip("Not testing stop as connection reliability test is probably starting")
+		}
 		response := helpers.CurlApp(appName, "/stop")
 		Expect(response).To(Equal("OK"))
 	})
