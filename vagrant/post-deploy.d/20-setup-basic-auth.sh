25c7ea48692717d23470fdfa3576d076ab21a396
Hector Rivas Gandara <keymon@gmail.com>
2016-05-10 17:39:54 +0100

    Use GDS custom concourse-lite AMI (us-east-1)
    Use the custom AMI with concourse-lite implemented in [1].
    
    With this new AMI:
     * It is not required to setup the ephemeral disk, as the AMI already
       uses /mnt/concourse for the worker state.
     * It is not required to add custom resources, as since concourse 1.0
       they can be defined in the pipeline itself.
    
    Pending: Use a AMI in eu-west-1, we created it in us-east-1 to reuse
    existing subnets and security groups.

--- vagrant/post-deploy.d/20-setup-basic-auth.sh
+++ vagrant/post-deploy.d/20-setup-basic-auth.sh
@@ -2,8 +2,7 @@
 set -u
 set -e
 echo "Setting up concourse basic auth as ${CONCOURSE_ATC_USER}: ${CONCOURSE_ATC_PASSWORD}"
-sed "s/--development-mode//g" -i /var/vcap/jobs/atc/bin/atc_ctl      # dev mode disables all auth
-sed "s/--basic-auth-username.*/--basic-auth-username \'${CONCOURSE_ATC_USER}\' \\\/" -i /var/vcap/jobs/atc/bin/atc_ctl
-sed "s/--basic-auth-password.*/--basic-auth-password \'${CONCOURSE_ATC_PASSWORD}\' \\\/" -i /var/vcap/jobs/atc/bin/atc_ctl
-/var/vcap/bosh/bin/monit restart atc
+sed "s/--basic-auth-username.*/--basic-auth-username \'${CONCOURSE_ATC_USER}\' \\\/" -i /etc/init/concourse-web.conf
+sed "s/--basic-auth-password.*/--basic-auth-password \'${CONCOURSE_ATC_PASSWORD}\' \\\/" -i /etc/init/concourse-web.conf
+service concourse-web restart
 
