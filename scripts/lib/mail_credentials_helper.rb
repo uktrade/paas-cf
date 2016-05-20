43792ab0898f055daef66f5286038ccb9603558f
Hector Rivas Gandara <keymon@gmail.com>
2016-05-20 11:49:16 +0100

    A ruby tool to synchronise admin users with CF
    This tool takes the list of users from config/admin_users.yml and
    synchronises them with the currently configured CF platform.
    
    - Any missing users are added to the UAA
    - New users are added as members of the groups defined in
      scripts/lib/uaa_sync_admin_users.rb
    - Any users in the admin groups but not in the admin_users.yml file will
      be removed if they are older than two hours - this is to allow the
      acceptance/smoke tests to create users during the test run
    - New users will recieve an email containing their admin login details
      encrypted to their GPG key (specified in admin_users.yml)
    
    admin_users.yml is a YAML file with a list of hashes containing:
    - email
    - gpg_key: to encrypt the generated credentials
    - username: optional, will default to email
    
    We use webmock to test the class mocking UAA. All the interactions can be
    recorded using `uaac` with the `--trace` option.
    
    We note that while the majority of this code has tests we've explicitly
    not tested the email sending code, as it is both extremely tricky and
    basically testing that SES actually works. We should assume that SES
    works.

---
+++ scripts/lib/mail_credentials_helper.rb
@@ -0,0 +1,88 @@
+require 'net/http'
+require 'uri'
+require 'tempfile'
+
+require 'aws-sdk'
+require 'mail'
+require 'gpgme'
+
+module EmailCredentialsHelper
+
+  DEFAULT_SOURCE_ADDRESS = "the-multi-cloud-paas-team@digital.cabinet-office.gov.uk"
+
+  def self.encrypt_message_to(message, key)
+    uri = URI.parse("http://pgp.mit.edu/pks/lookup?op=get&options=mr&search=0x" + key)
+    response = Net::HTTP.get_response(uri)
+    if response.code != "200"
+      raise "Cannot find GPG key #{key} in pgp.mit.edu"
+    end
+
+    Dir.mktmpdir("gpg-mail") do |tmpdir|
+      GPGME::Engine.home_dir = tmpdir
+      GPGME::Key.import(response.body)
+      GPGME::Crypto.encrypt(message, :recipients => key, :always_trust => true)
+    end
+  end
+
+  def self.send_email(opts)
+    ses = Aws::SES::Client.new(:region => "eu-west-1")
+    mail = Mail.new do
+      from    opts[:source_address]
+      to      opts[:email]
+      subject opts[:subject]
+
+      text_part do
+        body opts[:message]
+      end
+    end
+    opts[:attachments].each { | name, value |
+      mail.attachments[name] = value
+    }
+
+    ses.send_raw_email({
+      source: opts[:from],
+      destinations: [opts[:to]],
+      raw_message: {
+        data: mail.to_s,
+      },
+    })
+  end
+
+  def self.send_admin_credentials(api_url, user, source_address=nil)
+    source_address ||= DEFAULT_SOURCE_ADDRESS
+
+    attachment_source = encrypt_message_to(user[:password], user[:gpg_key])
+
+    send_email(
+      :from           => source_address,
+      :to             => user[:email],
+      :subject        => "PaaS admin account credentials",
+      :message        => %Q{
+Hello,
+
+A CF admin user has been created for you with the following details:
+
+ - API url: #{api_url}
+ - Login: #{user[:username]}
+ - Password: Attached as a GPG encrypted file with key ID #{user[:gpg_key]}
+
+You can login and change your password executing:
+
+  gpg -d < paas_admin_password.gpg
+  cf login -a #{api_url} -u #{user[:username]}
+  cf passwd
+
+Regards,
+The Government PaaS Team.
+
+... We are building a World Class Goverment PaaS based on OpenSource... that's the best!!!
+      },
+      :attachments    =>  {
+        'paas_admin_password.gpg' => {
+          :mime_type => "text/PGP; charset=binary",
+          :content   => attachment_source.to_s
+        }
+      }
+    )
+  end
+end
