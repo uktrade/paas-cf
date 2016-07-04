36f735396dd8ce42a82a7c1b64c6350dc1b61cd6
Colin Saliceti <c.saliceti@gmail.com>
2016-07-04 15:34:08 +0100

    Grafana dashboards to yaml

---
+++ manifests/cf-manifest/scripts/grafana-dashboards-manifest.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+require 'yaml'
+require 'json'
+
+dashboards_dir=ARGV[0]
+dashboard_files=Dir[dashboards_dir + '/*.json']
+dashboards_hash = { 'properties' => { 'grafana' => 'dashboards' } }
+
+dashboard_files.each{ |dashboard_file|
+  json = File.read(dashboard_file)
+  dashboards_hash['properties']['grafana']['dashboards'] = JSON.load(json)
+}
+
+File.write("grafana-dasbboards-manifest.yml", YAML.dump(content, f))
+
