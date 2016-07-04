795abd6baac99fa643860863d6554788e08b215c
Colin Saliceti <c.saliceti@gmail.com>
2016-07-04 16:17:07 +0100

    fix

--- manifests/cf-manifest/scripts/grafana-dashboards-manifest.rb
+++ manifests/cf-manifest/scripts/grafana-dashboards-manifest.rb
@@ -5,11 +5,11 @@ require 'json'
 
 dashboards_dir=ARGV[0]
 dashboard_files=Dir[dashboards_dir + '/*.json']
-dashboards_hash = { 'properties' => { 'grafana' => 'dashboards' } }
+dashboards_hash = { 'properties' => { 'grafana' => {'dashboards' => [] }}}
 
 dashboard_files.each{ |dashboard_file|
   json = File.read(dashboard_file)
-  dashboards_hash['properties']['grafana']['dashboards'] = JSON.load(json)
+  dashboards_hash['properties']['grafana']['dashboards'] << JSON.load(json)
 }
 
 puts dashboards_hash.to_yaml
