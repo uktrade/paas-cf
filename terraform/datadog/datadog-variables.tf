a517b05a190834a9b7ce78b05c95cae3ecbc9167
benhyland <ben.hyland@digital.cabinet-office.gov.uk>
2016-09-30 14:57:48 +0100

    Restructure datadog terraform config
    We are about to start writing a bunch of datadog checks.
    
    Restructure beforehand to establish a convention that
    each check goes into its own file.
    
    Make a home for variables, and extract variable for
    the deploy env into a `null_data_source` to establish
    a convention for working around terraform's
    inability to interpolate in variable declarations.
    See https://github.com/hashicorp/terraform/issues/4084

---
+++ terraform/datadog/datadog-variables.tf
@@ -0,0 +1,8 @@
+variable "datadog_api_key" {}
+variable "datadog_app_key" {}
+variable "env" {}
+
+# Work around https://github.com/hashicorp/terraform/issues/4084
+data "null_data_source" "datadog" {
+  env = "${replace(var.env, "-", "_")}"
+}
