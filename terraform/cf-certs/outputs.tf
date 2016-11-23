003eda80580dd6664875bf59ea4e44de71b7025b
Hector Rivas Gandara <keymon@gmail.com>
2016-11-23 14:52:11 +0000

    Create alternate certs for cloudfront
    Cloudfront requires certs with path set to /cloudfront/....
    
    With terraform the patch cannot be updated, so we create a new cert.

--- terraform/cf-certs/outputs.tf
+++ terraform/cf-certs/outputs.tf
@@ -13,3 +13,19 @@ output "apps_domain_cert_arn" {
 output "apps_domain_cert_id" {
   value = "${aws_iam_server_certificate.apps.id}"
 }
+
+output "system_domain_cloudfront_cert_arn" {
+  value = "${aws_iam_server_certificate.system_cloudfront.arn}"
+}
+
+output "system_domain_cloudfront_cert_id" {
+  value = "${aws_iam_server_certificate.system_cloudfront.id}"
+}
+
+output "apps_domain_cloudfront_cert_arn" {
+  value = "${aws_iam_server_certificate.apps_cloudfront.arn}"
+}
+
+output "apps_domain_cloudfront_cert_id" {
+  value = "${aws_iam_server_certificate.apps_cloudfront.id}"
+}
--- terraform/cf-certs/upload.tf
+++ terraform/cf-certs/upload.tf
@@ -7,7 +7,6 @@ resource "aws_iam_server_certificate" "system" {
   certificate_body  = "${var.system_domain_crt}"
   private_key       = "${var.system_domain_key}"
   certificate_chain = "${var.system_domain_intermediate_crt}"
-  path              = "/cloudfront/system/"
 
   lifecycle {
     create_before_destroy = true
@@ -15,6 +14,29 @@ resource "aws_iam_server_certificate" "system" {
 }
 
 resource "aws_iam_server_certificate" "apps" {
+  name_prefix       = "${var.env}-apps-domain-"
+  certificate_body  = "${var.apps_domain_crt}"
+  private_key       = "${var.apps_domain_key}"
+  certificate_chain = "${var.apps_domain_intermediate_crt}"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_iam_server_certificate" "system_cloudfront" {
+  name_prefix       = "${var.env}-system-domain-"
+  certificate_body  = "${var.system_domain_crt}"
+  private_key       = "${var.system_domain_key}"
+  certificate_chain = "${var.system_domain_intermediate_crt}"
+  path              = "/cloudfront/system/"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_iam_server_certificate" "apps_cloudfront" {
   name_prefix       = "${var.env}-apps-domain-"
   certificate_body  = "${var.apps_domain_crt}"
   private_key       = "${var.apps_domain_key}"
