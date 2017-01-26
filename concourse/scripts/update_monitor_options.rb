916ef0c8f39294d57c6614a9ad6a4916896ae11c
Piotr Komborski <piotr.komborski@gmail.com>
2017-01-26 15:50:20 +0000

    Update DD monitor options
    Since we have discovered that go-datadog library has problems with
    setting default options for some monitor
    settings(https://github.com/zorkian/go-datadog-api/issues/56) we need to
    use this script to ensure all options that we care about have desired
    values.

---
+++ concourse/scripts/update_monitor_options.rb
@@ -0,0 +1,53 @@
+#!/usr/bin/env ruby
+
+require 'json'
+require 'dogapi'
+
+tfstate = JSON.load($stdin)
+
+def to_boolean(str)
+  str == 'true'
+end
+
+def find_monitors(tfstate)
+  monitor_ids = Hash.new
+  tfstate['modules'][0]['resources'].each do |_, name|
+    attributes = Hash.new
+    if name["type"] == "datadog_monitor"
+      name["primary"]["attributes"].each do |attribute, value|
+        # Optimise for require_full_window attribute only as others have sane defaults
+        if attribute == "require_full_window" && value == "false"
+          attributes[attribute] = to_boolean(value)
+        end
+      end
+      if attributes != {}
+        attributes["query"] = name["primary"]["attributes"]["query"]
+        monitor_ids[name["primary"]["id"]] = attributes
+      end
+    end
+  end
+  monitor_ids
+end
+
+def update_monitors(monitor_ids, api_client)
+  monitor_ids.each do |id, options|
+    query = options["query"]
+    options.delete("query")
+    printf("Updating monitor %s with attributes %s\n", id, options)
+    resp = api_client.update_monitor(id, query, options: options)
+    if resp[0] != "200"
+      return resp
+    end
+  end
+  nil
+end
+
+api_key = ENV['TF_VAR_datadog_api_key']
+app_key = ENV['TF_VAR_datadog_app_key']
+api_host = ARGV[0]
+dog = Dogapi::Client.new(api_key, app_key, nil, nil, nil, nil, api_host)
+monitor_ids = find_monitors(tfstate)
+err = update_monitors(monitor_ids, dog)
+if err != nil
+  printf("Error: %s", err)
+end
