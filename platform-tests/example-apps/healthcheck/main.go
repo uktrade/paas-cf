b8def5087d4fddc21c451c260fc1185d3d189a71
bandesz <andras.ferencz-szabo@digital.cabinet-office.gov.uk>
2017-12-05 09:43:16 +0000

    Add graceful shutdown for the health check app
    Also add an endpoint for testing long response times + change the health
    checking to http in the manifest.
    
    This is for testing how the application behaves when it is stopped (502
    investigation).

--- platform-tests/example-apps/healthcheck/main.go
+++ platform-tests/example-apps/healthcheck/main.go
@@ -1,34 +1,75 @@
 package main
 
 import (
+	"context"
 	"crypto/tls"
 	"crypto/x509"
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
 	addr := ":" + os.Getenv("PORT")
+	srv := &http.Server{
+		Addr:         addr,
+		ReadTimeout:  30 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
 	fmt.Println("Listening on", addr)
+
 	http.HandleFunc("/", staticHandler)
+	http.HandleFunc("/long-response", longResponseHandler)
 	http.HandleFunc("/db", dbHandler)
 	http.HandleFunc("/mongo-test", mongoHandler)
 	http.HandleFunc("/elasticsearch-test", elasticsearchHandler)
 	http.HandleFunc("/redis-test", redisHandler)
 	http.HandleFunc("/compose-redis-test", composeRedisHandler)
-	err := http.ListenAndServe(addr, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+	defer signal.Reset(syscall.SIGINT, syscall.SIGTERM)
+
+	shutdownComplete := make(chan bool, 1)
+
+	go func() {
+		signal := <-signalChan
+		fmt.Printf("Received %s signal, shutting down\n", signal)
+		_, shutdown := context.WithTimeout(context.Background(), 30*time.Second)
+		defer shutdown()
+		/*err := srv.Shutdown(ctx)
+		if err != nil {
+			fmt.Printf("Shutdown returned an error: %s\n", err.Error())
+		}*/
+		fmt.Println("Shutdown complete")
+		shutdownComplete <- true
+	}()
+
+	go func() {
+		err := srv.ListenAndServe()
+		if err != http.ErrServerClosed {
+			fmt.Printf("ListenAndServe returned an error: %s\n", err.Error())
+		}
+	}()
+
+	<-shutdownComplete
+	fmt.Println("Exiting")
 }
 
 func staticHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Cache-Control", "max-age=0,no-store,no-cache")
 	http.ServeFile(w, r, "static/"+r.URL.Path[1:])
+}
+
+func longResponseHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Cache-Control", "max-age=0,no-store,no-cache")
+	time.Sleep(10 * time.Second)
+	http.ServeFile(w, r, "static/index.html")
 }
 
 func writeJson(w http.ResponseWriter, data interface{}) {
