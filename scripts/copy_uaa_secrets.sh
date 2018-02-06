093088a5a3d5e968f0615d880bef7c6b0c210782
bandesz <andras.ferencz-szabo@digital.cabinet-office.gov.uk>
2018-02-06 17:16:11 +0000

    Also copy admin and cc client secrets

--- scripts/copy_uaa_secrets.sh
+++ scripts/copy_uaa_secrets.sh
@@ -5,8 +5,8 @@ set -euo pipefail
 aws s3 cp s3://gds-paas-${FROM}-state/cf-secrets.yml cf-secrets-${FROM}.yml
 aws s3 cp s3://gds-paas-${TO}-state/cf-secrets.yml cf-secrets-${TO}.yml
 
-grep -v "uaa_clients" cf-secrets-${TO}.yml > cf-secrets-${TO}-updated.yml
-grep "uaa_clients" cf-secrets-${FROM}.yml >> cf-secrets-${TO}-updated.yml
+grep -v -E "uaa_clients|client_secret" cf-secrets-${TO}.yml > cf-secrets-${TO}-updated.yml
+grep -E "uaa_clients|client_secret" cf-secrets-${FROM}.yml >> cf-secrets-${TO}-updated.yml
 
 aws s3 cp cf-secrets-${TO}-updated.yml s3://gds-paas-${TO}-state/cf-secrets.yml
 
