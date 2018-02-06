619b6e862ee4e60b080ead6153e69361898e9355
bandesz <andras.ferencz-szabo@digital.cabinet-office.gov.uk>
2018-02-06 16:21:43 +0000

    Revert "Copy the bosh CA as the UAA CA to the second deployment"
    This reverts commit 4cddd558989e6a724fbf921aedfdb2a38e9f1b5f.

--- scripts/copy_uaa_secrets.sh
+++ scripts/copy_uaa_secrets.sh
@@ -2,15 +2,6 @@
 
 set -euo pipefail
 
-WORKING_DIR="$(mktemp -dt generate-cf-certs.XXXXXX)"
-trap 'rm -rf "${WORKING_DIR}"' EXIT
-
-echo "Working directory is ${WORKING_DIR}"
-
-cd ${WORKING_DIR}
-
-mkdir certs
-
 aws s3 cp s3://gds-paas-${FROM}-state/cf-secrets.yml cf-secrets-${FROM}.yml
 aws s3 cp s3://gds-paas-${TO}-state/cf-secrets.yml cf-secrets-${TO}.yml
 
@@ -19,17 +10,4 @@ grep "uaa_clients" cf-secrets-${FROM}.yml >> cf-secrets-${TO}-updated.yml
 
 aws s3 cp cf-secrets-${TO}-updated.yml s3://gds-paas-${TO}-state/cf-secrets.yml
 
-aws s3 cp s3://gds-paas-${FROM}-state/bosh-CA.tar.gz .
-aws s3 cp s3://gds-paas-${TO}-state/cf-certs.tar.gz .
-
-tar xzf bosh-CA.tar.gz
-tar xzf cf-certs.tar.gz -C certs
-
-rm cf-certs.tar.gz
-
-mv bosh-CA.crt certs/uaa-CA.crt
-echo "" > certs/uaa-CA.key
-
-tar czf cf-certs.tar.gz -C certs .
-
-aws s3 cp cf-certs.tar.gz s3://gds-paas-${TO}-state/cf-certs.tar.gz
+rm cf-secrets-*
