6d23dc0931bdfba3e06c33352e4caff87a815212
Andrew Bruce <me@andrewbruce.net>
2018-03-23 15:18:48 +0000

    Can calculate RDS prices for single/multi AZ

---
+++ Makefile
@@ -0,0 +1,17 @@
+rds_prices.yml: 050-rds-broker.yml rds_price_list.dump
+	bundle exec ./get_prices.rb \
+	  050-rds-broker.yml \
+	  < prod-plans.json \
+	  > rds_prices.yml
+
+050-rds-broker.yml:
+	curl https://raw.githubusercontent.com/alphagov/paas-cf/master/manifests/cf-manifest/manifest/050-rds-broker.yml \
+	  > $@
+
+rds_price_list.dump:
+	bundle exec ruby \
+	  -r amazon-pricing \
+	  -e "pl=AwsPricing::RdsPriceList.new;File.write('$@', Marshal.dump(pl))"
+
+clean:
+	rm -f 050-rds-broker.yml rds_price_list.dump rds_prices.yml
--- get_prices.rb
+++ get_prices.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
 require 'yaml'
+require 'amazon-pricing'
 
 broker_manifest_path = ARGV[0]
 
@@ -12,37 +13,52 @@ broker_manifest_path = ARGV[0]
 
 def plans(manifest, plan, job, properties_key, service)
   manifest['instance_groups'].
-    detect { |p| p['name'] == plan }['jobs'].
+    detect { |p| p['name'] == plan }.
+    fetch('jobs').
     detect { |j| j['name'] == job }.
-    fetch('properties')[properties_key]['catalog']['services'].
+    fetch('properties').
+    fetch(properties_key).
+    fetch('catalog').
+    fetch('services').
     detect { |s| s['name'] == service }.
     fetch('plans')
 end
 
-def calculate(plans, manifest)
-  broker_plans = plans(
+def calculate(prices, plans, manifest)
+  plans(
     manifest,
     'rds_broker',
     'rds-broker',
     'rds-broker',
     'postgres'
-  )
-  plans.map { |plan|
-    plan_id = plan['entity']['unique_id']
-    broker_plan = broker_plans.
-      detect { |p| p['id'] == plan_id }
-
-    raise "no broker plan for id #{plan_id}:\n#{plan.to_yaml}" if broker_plan.nil?
-
+  ).map { |plan|
+    plan_id = plan['id']
+    api_plan = plans.detect { |p| p['entity']['unique_id'] == plan_id }
+    if api_plan.nil?
+      raise "no api plan for id #{plan_id}:\n#{plan.to_yaml}"
+    end
+    db_instance_class = plan['rds_properties']['db_instance_class']
+    rds_instance_type = prices.rds_instance_types.
+      detect { |t| t.api_name == db_instance_class }
+    is_multi_az = plan['rds_properties']['multi_az']
+    price = rds_instance_type.price_per_hour(
+      :postgresql,
+      :ondemand,
+      _term = nil,
+      is_multi_az,
+    )
     {
-      name: broker_plan['name'],
+      name: plan['name'],
+      multi_az: is_multi_az,
+      instance_class: db_instance_class,
       plan_guid: plan_id,
-      price_per_hour: 0,
+      price_per_hour: price,
     }
   }
 end
 
+rds_price_list = Marshal.load(File.read('rds_price_list.dump'))
 input_plans = YAML.safe_load($stdin)
 broker_plans = YAML.load_file(broker_manifest_path)
 
-puts calculate(input_plans, broker_plans).to_yaml
+puts calculate(rds_price_list.get_region('eu-west-1'), input_plans, broker_plans).to_yaml
