3938a17171081341c9bb0e84ee83c505670a9daa
Rafal Proszowski <rafal.proszowski@digital.cabinet-office.gov.uk>
2018-04-20 11:57:44 +0100

    Use log-cache release

--- manifests/cf-manifest/scripts/generate-manifest.sh
+++ manifests/cf-manifest/scripts/generate-manifest.sh
@@ -36,6 +36,7 @@ bosh interpolate \
   --ops-file="${PAAS_CF_DIR}/manifests/cf-manifest/manifest/operations/070-elasticache-broker.yml" \
   --ops-file="${PAAS_CF_DIR}/manifests/cf-manifest/manifest/operations/080-logsearch.yml" \
   --ops-file="${PAAS_CF_DIR}/manifests/cf-manifest/manifest/operations/110-metrics.yml" \
+  --ops-file="${PAAS_CF_DIR}/manifests/cf-manifest/manifest/operations/150-log-cache.yml" \
   --ops-file="${PAAS_CF_DIR}/manifests/cf-manifest/manifest/operations/200-paas-admin-uaa-client.yml" \
   --ops-file="${WORKDIR}/grafana-dashboards-opsfile/grafana-dashboards-opsfile.yml" \
   --ops-file="${WORKDIR}/vpc-peering-opsfile/vpc-peers.yml" \
