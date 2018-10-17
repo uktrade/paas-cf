bef65244d008105fd961e665e7db057f4c104b7e
Hector Rivas Gandara <keymon@gmail.com>
2018-10-17 17:49:04 +0100

    Add ALB for gorouter for apps domain
    Initial implementation of the ALB. We add the new loadbalancer
    a listener with a default action to send to gorouter
    and the HTTP => HTTPS redirect.
    
    The gorouter group is using the port 80, non SSL, because
    currently we have the haproxy in port 443 in the gorouter VM
    and the gorouter is listening only HTTP.
    
    Note that the job in the routing-release do not allow
    override the tls port yet, only in the develop branch
    https://github.com/cloudfoundry/routing-release/commit/77f3b95cca2be066d5720d23377c6b5478b80af8

---
+++ terraform/cloudfoundry/albs.tf
@@ -0,0 +1,79 @@
+resource "aws_lb" "cf_router_alb" {
+  name               = "${var.env}-cf-router"
+  internal           = false
+  load_balancer_type = "application"
+  subnets            = ["${split(",", var.infra_subnet_ids)}"]
+  security_groups    = ["${aws_security_group.web.id}"]
+
+  idle_timeout                      = "${var.elb_idle_timeout}"
+  enable_cross_zone_load_balancing  = "true"
+
+  access_logs {
+    enabled = true
+    bucket  = "${aws_s3_bucket.elb_access_log.id}"
+    prefix  = "cf-router-alb"
+  }
+}
+
+resource "aws_lb_target_group" "cf_router" {
+  name     = "${var.env}-cf-router-target-group"
+  # FIXME: do SSL to gorouter. To do that we need to setup the
+  # Go Router to enable the HTTPS endpoint, but also change the tls_port
+  # to avoid conflicts with haproxy.
+  # tls_port cannot be changed until the latest version of routing_release
+  #port     = "8443"
+  #protocol = "HTTPS"
+  port     = "80"
+  protocol = "HTTP"
+  vpc_id   = "${var.vpc_id}"
+
+  health_check {
+    protocol            = "HTTP"
+    port                = "8080"
+    path                = "/health"
+    matcher             = "200"
+    interval            = "${var.health_check_interval}"
+    timeout             = "${var.health_check_timeout}"
+    healthy_threshold   = "${var.health_check_healthy}"
+    unhealthy_threshold = "${var.health_check_unhealthy}"
+  }
+}
+
+resource "aws_lb_ssl_negotiation_policy" "cf_router_alb" {
+  name          = "paas-${var.default_elb_security_policy}"
+  load_balancer = "${aws_lb.cf_router_alb.name}"
+  lb_port       = 443
+
+  attribute {
+    name  = "Reference-Security-Policy"
+    value = "${var.default_elb_security_policy}"
+  }
+}
+
+resource "aws_lb_listener" "cf_router_apps_listener_443" {
+  load_balancer_arn = "${aws_lb.cf_router_alb.arn}"
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2015-05"
+  certificate_arn   = "${data.aws_acm_certificate.apps.arn}"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.cf_router.arn}"
+  }
+}
+
+resource "aws_lb_listener" "cf_router_listener_80" {
+  load_balancer_arn = "${aws_lb.cf_router_alb.arn}"
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+    redirect {
+      port = "443"
+      protocol = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
