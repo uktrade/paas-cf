552569bee11c300c09019f9a17725c4a31e2c290
Lee Porte <lee.porte@digital.cabinet-office.gov.uk>
2019-01-08 13:49:26 +0000

    Add CI/CD loadbalancer

--- terraform/cloudfoundry/ci-cd_broker.tf
+++ terraform/cloudfoundry/ci-cd_broker.tf
@@ -12,3 +12,45 @@ resource "aws_iam_user_group_membership" "ci-cd_broker" {
 resource "aws_iam_access_key" "ci-cd_broker" {
   user = "${aws_iam_user.ci-cd_broker.name}"
 }
+
+resource "aws_elb" "ci-cd_broker" {
+  name                      = "${var.env}-ci-cd-broker"
+  subnets                   = ["${split(",", var.infra_subnet_ids)}"]
+  idle_timeout              = "${var.elb_idle_timeout}"
+  cross_zone_load_balancing = "true"
+  internal                  = true
+  security_groups           = ["${aws_security_group.ci-cd_broker.id}"]
+
+  access_logs {
+    bucket        = "${aws_s3_bucket.elb_access_log.id}"
+    bucket_prefix = "cf-broker-ci-cd"
+    interval      = 5
+  }
+
+  health_check {
+    target              = "HTTP:80/healthcheck"
+    interval            = "${var.health_check_interval}"
+    timeout             = "${var.health_check_timeout}"
+    healthy_threshold   = "${var.health_check_healthy}"
+    unhealthy_threshold = "${var.health_check_unhealthy}"
+  }
+
+  listener {
+    instance_port      = 80
+    instance_protocol  = "http"
+    lb_port            = 443
+    lb_protocol        = "https"
+    ssl_certificate_id = "${data.aws_acm_certificate.system.arn}"
+  }
+}
+
+resource "aws_lb_ssl_negotiation_policy" "ci-cd_broker" {
+  name          = "paas-${var.default_elb_security_policy}"
+  load_balancer = "${aws_elb.ci-cd_broker.id}"
+  lb_port       = 443
+
+  attribute {
+    name  = "Reference-Security-Policy"
+    value = "${var.default_elb_security_policy}"
+  }
+}
--- terraform/cloudfoundry/security-groups.tf
+++ terraform/cloudfoundry/security-groups.tf
@@ -156,6 +156,16 @@ resource "aws_security_group" "ci-cd_broker" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+
+    security_groups = [
+      "${aws_security_group.cloud_controller.id}",
+    ]
+  }
+
   tags {
     Name = "${var.env}-ci-cd-brokers"
   }
