930752ad9b968377da8544c7e6cb7c928cfd2e3d
Tom Whitwell <tom.whitwell@digital.cabinet-office.gov.uk>
2019-01-18 17:04:27 +0000

    Make buildpack update process automatic
    Updating the buildpacks can now be done with `update_buildpacks.sh`.
    
    This script runs go code at `tools/buildpacks` which:
    
    1. Parses the buildpacks.yml file
    2. Contacts the GitHub API to find the latest version for each of the configured buildpacks
    3. Updates buildpacks.yml with these latest versions

---
+++ tools/buildpacks/main.go
@@ -0,0 +1,197 @@
+package main
+
+import (
+	"context"
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/google/go-github/v21/github"
+	"golang.org/x/oauth2"
+	yaml "gopkg.in/yaml.v2"
+)
+
+type AssetDetails struct {
+	Url      string
+	Filename string
+	Sha      string
+}
+
+type DefaultVersion struct {
+	Name    string `yaml:"name"`
+	Version string `yaml:"version"`
+}
+
+type Manifest struct {
+	DefaultVersions []DefaultVersion `yaml:"default_versions"`
+	Dependencies    []Dependency     `yaml:"dependencies"`
+}
+
+func downloadFile(filepath string, url string) error {
+
+	// Create the file
+	out, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	// Get the data
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Write the body to file
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func getAssetSha(assetDetails AssetDetails) (sha string) {
+	tmpDir, err := ioutil.TempDir("/tmp", "create_buildpacks")
+	if err != nil {
+		log.Fatalf("Temporary download dir could not be created %v", err)
+	}
+	downloadFileName := fmt.Sprintf("%s/%s", tmpDir, assetDetails.Filename)
+	err = downloadFile(downloadFileName, assetDetails.Url)
+	if err != nil {
+		log.Fatalf("File %s could not be downloaded to %s %v", assetDetails.Url, downloadFileName, err)
+	}
+	f, err := os.Open(downloadFileName)
+	if err != nil {
+		log.Fatalf("File %s could not be opened %v", downloadFile, err)
+	}
+	defer f.Close()
+	hasher := sha256.New()
+	if _, err := io.Copy(hasher, f); err != nil {
+		log.Fatalf("Calculating SHA256SUM of %s failed %v", downloadFileName, err)
+	}
+	return hex.EncodeToString(hasher.Sum(nil))
+}
+
+func readManifest(githubClient *github.Client, buildpack Buildpack, ctx context.Context) (manifest Manifest) {
+	fileContent, _, _, err := githubClient.Repositories.GetContents(
+		ctx,
+		"cloudfoundry",
+		buildpack.RepoName,
+		"manifest.yml",
+		&github.RepositoryContentGetOptions{},
+	)
+	if err != nil {
+		// log.Fatalf("could not get contents for manifest for %s, %v", buildpack.RepoName, err)
+		return manifest
+	}
+	fileBytes, err := fileContent.GetContent()
+	if err != nil {
+		log.Fatalf("could not read contents for manifest for %s, %v", buildpack.RepoName, err)
+	}
+	err = yaml.Unmarshal([]byte(fileBytes), &manifest)
+	if err != nil {
+		log.Fatalf("could not unmarshal manifest as YAML, %v", err)
+	}
+	return manifest
+}
+
+func downloadSha(url, repoName string) (shasum string) {
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatalf("could not download shasum for %s, %v", repoName, err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("could not read response for shasum for %s, %v", repoName, err)
+	}
+	return strings.Split(string(body), " ")[0]
+}
+
+func getAssetDetails(assets []github.ReleaseAsset, repoName, nameFilter string) (assetDetails AssetDetails, ok bool) {
+	ok = false
+	for _, asset := range assets {
+		assetName := *asset.Name
+		if nameFilter != "" && !strings.Contains(assetName, nameFilter) {
+			continue
+		}
+		if strings.HasSuffix(assetName, ".zip") {
+			assetDetails.Url = *asset.BrowserDownloadURL
+			assetDetails.Filename = assetName
+			ok = true
+		}
+		if strings.HasSuffix(assetName, "SHA256SUM.txt") {
+			assetDetails.Sha = downloadSha(*asset.BrowserDownloadURL, repoName)
+		}
+	}
+	return assetDetails, ok
+}
+
+func main() {
+	stdin, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		log.Fatalf("couldn't read all of stdin, %v", err)
+	}
+	var result *Buildpacks
+	err = yaml.Unmarshal(stdin, &result)
+	if err != nil {
+		log.Fatalf("could not unmarshal YAML: %v", err)
+	}
+
+	ctx := context.Background()
+	githubToken, ok := os.LookupEnv("GITHUB_API_TOKEN")
+	if !ok {
+		log.Fatalf("environment variable GITHUB_API_TOKEN must be set")
+	}
+	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: githubToken})
+	tc := oauth2.NewClient(ctx, ts)
+	githubClient := github.NewClient(tc)
+	buildpackConfig := Buildpacks{}
+	for _, buildpack := range result.Buildpacks {
+		log.Printf("Processing %s (%s)\n", buildpack.Name, buildpack.Stack)
+		release, _, err := githubClient.Repositories.GetLatestRelease(ctx, "cloudfoundry", buildpack.RepoName)
+		if err != nil {
+			log.Fatalf("could not get latest release for %s, %v", buildpack.RepoName, err)
+		}
+
+		assetDetails, ok := getAssetDetails(release.Assets, buildpack.RepoName, buildpack.Stack)
+		if !ok {
+			assetDetails, ok = getAssetDetails(release.Assets, buildpack.RepoName, "")
+			if !ok {
+				log.Fatalf("could not find assets for release %s", *release.URL)
+			}
+		}
+
+		manifest := readManifest(githubClient, buildpack, ctx)
+		if assetDetails.Sha == "" {
+			log.Printf("SHA for %s %s could not be read. Downloading %s to determine correct SHA for buildpack config.\n", buildpack.Name, *release.TagName, assetDetails.Url)
+			assetDetails.Sha = getAssetSha(assetDetails)
+
+		}
+		newBuildpack := Buildpack{
+			Name:         buildpack.Name,
+			RepoName:     buildpack.RepoName,
+			Stack:        buildpack.Stack,
+			Filename:     assetDetails.Filename,
+			Sha:          assetDetails.Sha,
+			Url:          assetDetails.Url,
+			Version:      *release.TagName,
+			Dependencies: manifest.Dependencies,
+		}
+
+		buildpackConfig.Buildpacks = append(buildpackConfig.Buildpacks, newBuildpack)
+	}
+	newBuildpacksYaml, err := yaml.Marshal(buildpackConfig)
+	if err != nil {
+		log.Fatalf("could not marshal buildpackConfig into yaml, %v", err)
+	}
+	fmt.Print(string(newBuildpacksYaml))
+}
