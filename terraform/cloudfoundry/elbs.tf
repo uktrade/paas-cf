450b893b1c328e46b0d767bec343a63a3f43e526
Lee Porte <lee.porte@digital.cabinet-office.gov.uk>
2019-04-02 10:31:13 +0100

    added elb terraform for istio

--- terraform/cloudfoundry/elbs.tf
+++ terraform/cloudfoundry/elbs.tf
@@ -170,3 +170,66 @@ resource "aws_elb" "ssh_proxy" {
     lb_protocol       = "tcp"
   }
 }
+
+resource "aws_elb" "cf-istio-router" {
+  name                      = "${var.env}-cf-istio-router"
+  subnets                   = ["${split(",", var.infra_subnet_ids)}"]
+  idle_timeout              = "${var.elb_idle_timeout}"
+  cross_zone_load_balancing = "true"
+
+  security_groups = ["${aws_security_group.web.id}"]
+
+  access_logs {
+    bucket        = "${aws_s3_bucket.elb_access_log.id}"
+    bucket_prefix = "cf-router"
+    interval      = 5
+  }
+
+  health_check {
+    target              = "HTTP:8002/healthcheck"
+    interval            = "${var.health_check_interval}"
+    timeout             = "${var.health_check_timeout}"
+    healthy_threshold   = "${var.health_check_healthy}"
+    unhealthy_threshold = "${var.health_check_unhealthy}"
+  }
+
+  listener {
+    instance_port      = 443
+    instance_protocol  = "ssl"
+    lb_port            = 443
+    lb_protocol        = "ssl"
+    ssl_certificate_id = "${aws_acm_certificate_validation.apps.certificate_arn}"
+  }
+
+  listener {
+    lb_port           = "80"
+    lb_protocol       = "http"
+    instance_port     = "80"
+    instance_protocol = "http"
+  }
+}
+
+
+resource "google_compute_address" "cf-istio-router" {
+  name = "${var.env}-cf-istio-router"
+}
+
+resource "google_dns_record_set" "cf-istio-router-dns" {
+  name       = "*.istio.${google_dns_managed_zone.env_dns_zone.dns_name}"
+  depends_on = ["google_compute_address.cf-istio-router"]
+  type       = "A"
+  ttl        = 300
+
+  managed_zone = "${google_dns_managed_zone.env_dns_zone.name}"
+
+  rrdatas = ["${google_compute_address.cf-istio-router.address}"]
+}
+
+
+output "istio_router_lb_ip" {
+  value = "${google_compute_address.cf-istio-router.address}"
+}
+
+output "istio_router_target_pool" {
+  value = "${google_compute_target_pool.cf-istio-router.name}"
+}
